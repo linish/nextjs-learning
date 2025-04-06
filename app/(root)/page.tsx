44f90396f585232e37997e129750f0d23de9834b
Linish V S <linish@riefmedia.com>
2025-04-06 10:22:31 +0530

    Error handling - April 6, 2025

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import Hello from "@/app/components/hello";
 
 export default function Home() {
   console.log("Home page - Server / Client Component");
-  return (
+    return (
       <>
         <h3 className="text-3xl">Welcome to NextJS</h3>
         <Hello />
---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div>
+            <h1>Loading Please Wait...</h1>
+        </div>
+    );
+};
+
+export default Loading;
